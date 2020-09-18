4f2e2c2f30f3a62535d0d27552fd13b3e6a263ed
Veeral Patel <vpatel98@gmail.com>
2020-09-17 20:14:27 -0700

    further reduce dependencies

--- src/lib/base64.ts
+++
@@ -1,5 +0,0 @@
-// Decodes a base64 encoded string
-export function base64Decode(input: string): string {
-  let buff = new Buffer(input, 'base64');
-  return buff.toString('ascii');
-}
--- src/lib/extract_urls.ts
+++ src/lib/extract_urls.ts
@@ -1,7 +1,97 @@
 import Bluebird from 'bluebird';
 import getUrls from 'get-urls';
 import { gmail_v1 } from 'googleapis';
-import { getText } from './text';
+
+// Decodes a base64 encoded string
+function base64Decode(input: string): string {
+  let buff = new Buffer(input, 'base64');
+  return buff.toString('ascii');
+}
+
+async function getAttachment(
+  gmail: gmail_v1.Gmail,
+  messageId: string,
+  attachmentId: string
+): Promise<gmail_v1.Schema$MessagePartBody | null> {
+  const response = await gmail.users.messages.attachments.get({
+    userId: 'me',
+    messageId: messageId,
+    id: attachmentId,
+  });
+
+  return response.data;
+}
+
+// combined text from the plain, html parts
+async function getText(
+  gmail: gmail_v1.Gmail,
+  messageId: string,
+  payload: gmail_v1.Schema$MessagePart
+): Promise<string> {
+  if (!payload?.parts) {
+    if (payload?.body?.data) {
+      if (
+        payload.mimeType === 'text/plain' ||
+        payload.mimeType === 'text/html'
+      ) {
+        return base64Decode(payload.body.data);
+      }
+    } else {
+      return '';
+    }
+  }
+
+  var initialText: string = '';
+  if (payload.body?.data) {
+    initialText = base64Decode(payload.body?.data);
+  }
+
+  let piecesOfText: string[] = [];
+  if (payload.parts)
+    piecesOfText = await Bluebird.map(payload.parts, async (part) => {
+      let text: string = '';
+
+      if (part.filename) {
+        // if this part represents an attachment, get the text from the attachment too!
+        if (part.body?.attachmentId) {
+          if (part.mimeType === 'text/plain') {
+            const attachment = await getAttachment(
+              gmail,
+              messageId,
+              part.body.attachmentId
+            );
+
+            if (attachment?.data) {
+              text += base64Decode(attachment?.data);
+            }
+          }
+        } else if (part.body?.data) {
+          // base64 decode the attachment data from the part and then get text
+          if (part.mimeType === 'text/plain') {
+            if (part.body?.data) {
+              text += base64Decode(part.body?.data);
+            }
+          }
+        }
+      } else {
+        if (part.mimeType === 'text/plain' || part.mimeType === 'text/html') {
+          if (part.body?.data) {
+            text += base64Decode(part.body?.data);
+          }
+        } else {
+          // it's either a container part so we get the text from its subparts
+          // or it's a part we don't care about, which doesn't have sub-parts, so getText(...) will output an empty string
+
+          // also: wrap this await in a try/catch clause
+          text += await getText(gmail, messageId, part);
+        }
+      }
+      return text;
+    });
+
+  piecesOfText.push(initialText);
+  return piecesOfText.join('\n');
+}
 
 export async function getUrlsFromMessages(
   gmail: gmail_v1.Gmail,
