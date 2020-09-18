fec52f9156caf7e583664d4a4f1d3bcd81e0c57f
Veeral Patel <vpatel98@gmail.com>
2020-09-17 22:15:41 -0700

    extract out getUrlsFromMessage function

--- src/lib/extract_urls.ts
+++ src/lib/extract_urls.ts
@@ -2,30 +2,34 @@ import Bluebird from 'bluebird';
 import getUrls from 'get-urls';
 import { gmail_v1 } from 'googleapis';
 
-// [Testable]
+// [Not testing]
 export async function getUrlsFromMessages(
   gmail: gmail_v1.Gmail,
   messages: gmail_v1.Schema$Message[]
 ): Promise<string[]> {
-  const listOflistsOfUrls = await Bluebird.map(messages, async (message) => {
-    if (message?.id && message?.payload) {
-      // get the text from our email message
-      const text = await getText(gmail, message.id, message.payload);
-
-      // extract URLs from our text
-      const newUrls: string[] = Array.from(getUrls(text));
-
-      return newUrls;
-    } else {
-      return [];
-    }
-  });
+  const listOflistsOfUrls = await Bluebird.map(messages, async (message) =>
+    getUrlsFromMessage(gmail, message)
+  );
 
   let allUrls: string[] = [];
   listOflistsOfUrls.forEach((lst) => {
     if (lst) allUrls = allUrls.concat(lst);
   });
   return allUrls;
+}
+
+// [Testable]
+async function getUrlsFromMessage(
+  gmail: gmail_v1.Gmail,
+  message: gmail_v1.Schema$Message
+) {
+  if (!message.id || !message.payload) return [];
+
+  const text = await getText(gmail, message.id, message.payload);
+
+  const foundUrls: string[] = Array.from(getUrls(text));
+
+  return foundUrls;
 }
 
 // [Not testing]
