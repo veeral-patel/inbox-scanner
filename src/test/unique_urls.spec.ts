e88293aeefb6b19e38b475ceafc055565dc5b43d
Veeral Patel <vpatel98@gmail.com>
2020-09-18 13:45:53 -0700

    sanity test getUniqueUrls

--- src/test/unique_urls.spec.ts
+++ src/test/unique_urls.spec.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { stripQueryParameters } from '../lib/unique_urls';
+import { getUniqueUrls, stripQueryParameters } from '../lib/unique_urls';
 
 test('Test stripping query parameters', (t) => {
   t.is(
@@ -7,5 +7,19 @@ test('Test stripping query parameters', (t) => {
       'https://dropbox.com/scl/fi/m0q9rk3edqgtx6htguihx/Product-Frameworks.paper?dl=0&rlkey=1raczbqtantumxu8emnmgp72y'
     ),
     'https://dropbox.com/scl/fi/m0q9rk3edqgtx6htguihx/Product-Frameworks.paper'
+  );
+});
+
+test('Test getting unique URLs', (t) => {
+  t.deepEqual(
+    getUniqueUrls([
+      'https://docs.google.com/spreadsheets/abc',
+      'https://docs.google.com/spreadsheets/abc?q=hello',
+      'https://docs.google.com/spreadsheets/bcd?q=hello',
+    ]),
+    [
+      'https://docs.google.com/spreadsheets/abc',
+      'https://docs.google.com/spreadsheets/bcd',
+    ]
   );
 });
