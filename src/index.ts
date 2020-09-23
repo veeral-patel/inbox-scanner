02f240444e3510bae197bd870fe69ba87fc08bbc
Veeral Patel <vpatel98@gmail.com>
2020-09-22 17:40:48 -0700

    got oauth flow done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import fs from 'fs';
-import { gmail_v1 } from 'googleapis';
+import { GaxiosError } from 'gaxios';
+import { gmail_v1, google } from 'googleapis';
 import VError from 'verror';
 import { getUrlsFromMessages } from './lib/extract_urls';
 import { getFileUrls } from './lib/file_url';
 import { getMessages } from './lib/message';
-import { getAuthUrl } from './lib/oauth';
+import { getAuthUrl, getOAuthClient } from './lib/oauth';
 import { getPublicUrls } from './lib/public_file_url';
 import { getUniqueUrls } from './lib/unique_urls';
 
@@ -13,21 +14,70 @@ const PORT = 7777;
 
 const app = express();
 
+// Redirects you to a URL where you can log in and grant access via OAuth
 app.get('/', (_req, res) => {
   // Read our OAuth app credentials...
   fs.readFile('credentials.json', (err, content) => {
-    // And if we get an error, respond with a 500
+    // And if we get an error reading this file, respond with a 500, log the error to
+    // console, and shut down the server (as this error is un-recoverable.)
     if (err) {
       const wrappedError = new VError(
         err,
         "Failed to load client secret file. Please create a credentials.json file if one doesn't exist"
       );
-      console.error(wrappedError.message);
       res.sendStatus(500);
+
+      console.error(wrappedError.message);
+      process.exit();
     } else {
       // Otherwise, generate a URL for the user to authenticate at and redirect to that URL
       const authUrl = getAuthUrl(JSON.parse(content.toString()));
       res.redirect(authUrl);
+    }
+  });
+});
+
+// After you grant access successfully, Google redirects your browser to this callback URL.
+app.get('/callback', (req, res) => {
+  const code = req.query.code;
+  if (!code) {
+    res.send('Failed to get code from the callback URL');
+    return;
+  }
+
+  // TODO: don't duplicate all this code
+  // Read our OAuth app credentials (again)...
+  fs.readFile('credentials.json', (err, content) => {
+    // And if we get an error reading this file, respond with a 500, log the error to
+    // console, and shut down the server (as this error is un-recoverable.)
+    if (err) {
+      const wrappedError = new VError(
+        err,
+        "Failed to load client secret file. Please create a credentials.json file if one doesn't exist"
+      );
+      res.sendStatus(500);
+
+      console.error(wrappedError.message);
+      process.exit();
+    } else {
+      const oAuth2Client = getOAuthClient(JSON.parse(content.toString()));
+      oAuth2Client.getToken(
+        code as string,
+        (err: GaxiosError | null, token?: any) => {
+          if (err) {
+            res.send(`Error retrieving access token: ${err.message}`);
+            return;
+          } else {
+            oAuth2Client.setCredentials(token);
+
+            const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+            res.send('Scanning your emails now. Please visit your console.');
+
+            scanEmails(gmail);
+          }
+        }
+      );
     }
   });
 });
--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,17 +1,10 @@
-import { GaxiosError } from 'gaxios';
-import { gmail_v1, google } from 'googleapis';
+import { google } from 'googleapis';
 
 // The code is in this file is from Gmail's Node.js SDK quickstart.
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first time
-const TOKEN_PATH = 'token.json';
-
-// Create an OAuth2 client with the given credentials, and then execute the
-// given callback function [Not testing]
-export function getAuthUrl(credentials: any) {
+export function getOAuthClient(credentials: any) {
   const { client_secret, client_id, redirect_uris } = credentials.web;
 
   const oAuth2Client = new google.auth.OAuth2(
@@ -20,27 +13,18 @@ export function getAuthUrl(credentials: any) {
     redirect_uris[0]
   );
 
+  return oAuth2Client;
+}
+
+// Create an OAuth2 client with the given credentials, and then execute the
+// given callback function [Not testing]
+export function getAuthUrl(credentials: any) {
+  const oAuth2Client = getOAuthClient(credentials);
+
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
   });
 
   return authUrl;
-}
-
-function oAuthCodeReceived(
-  oAuth2Client: any,
-  code: string,
-  callback: (gmail: gmail_v1.Gmail) => void
-) {
-  oAuth2Client.getToken(code, (err: GaxiosError | null, token?: any) => {
-    if (err)
-      return console.error(`Error retrieving access token: ${err.message}`);
-    else {
-      oAuth2Client.setCredentials(token);
-
-      const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-      callback(gmail);
-    }
-  });
 }
